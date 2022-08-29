cf899348a51fb920df66fed7775f2ecd7469bed5
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2022-08-29 15:49:25 +0700

    Practicum 2

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Rajendra Rakha Arya Prabaswara',
             ),
             Text(
               '$_counter',
